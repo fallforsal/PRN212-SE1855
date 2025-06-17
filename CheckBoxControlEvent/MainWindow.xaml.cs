9d5ac4d81cd934fd56cd53cf679e37395c5fe674
Nguyễn Việt Hoàng <viethoan032005@gmail.com>
2025-06-17 10:14:47 +0700

    CheckBoxControl

---
+++ CheckBoxControlEvent/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBoxControlEvent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ChkToanBo_Checked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = true;
+            chkDaBong.IsChecked = true;
+            chkDulich.IsChecked = true;
+            chkXemPhim.IsChecked = true;
+        }
+        private void ChkToanBo_Unchecked(object sender, RoutedEventArgs e)
+        {
+            chkCongNghe.IsChecked = false;
+            chkDaBong.IsChecked = false;
+            chkDulich.IsChecked = false;
+            chkXemPhim.IsChecked = false;
+        }
+        private void chkSub_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            if (chkCongNghe.IsChecked == true && chkDaBong.IsChecked == true && chkDulich.IsChecked == true && chkXemPhim.IsChecked == true)
+            {
+                chkToanBo.IsChecked = true;
+            }
+            else
+            {
+                chkToanBo.IsChecked = false;
+            }
+        }
+    }
+}

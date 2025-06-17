db2b2d3d480f55654a1a8dcf2c867d4359bab70b
Nguyễn Việt Hoàng <viethoan032005@gmail.com>
2025-06-17 10:38:07 +0700

    Radio button

---
+++ RadioButtonControl/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioButtonControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void BtnGui_Click(object sender, RoutedEventArgs e)
+        {
+            string binhChon = "";
+            if (radKhongTot.IsChecked == true)
+                binhChon = radKhongTot.Content + "";
+            else if (radTot.IsChecked == true)
+                binhChon = radTot.Content + "";
+            else if (radRatTot.IsChecked == true)
+                binhChon = radRatTot.Content + "";
+            else if (radTamDuoc.IsChecked == true)
+                binhChon = radTamDuoc.Content + "";
+            string gioiTinh = "";
+            if (radNam.IsChecked == true)
+                gioiTinh = radNam.Content + "";
+            else if (radNu.IsChecked == true)
+                gioiTinh = radNu.Content + "";
+            string infor = "Bạn bình chọn hệ thống =" + binhChon + Environment.NewLine;
+            infor += "Giới tính của bạn là: " + gioiTinh;
+            MessageBoxResult ret = MessageBox.Show(infor, "Mời bạn xác nhận", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (ret == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Cảm ơn bạn đã bình chọn", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Bạn đã hủy bình chọn", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+        }
+        private void BtnHuy_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+    }
